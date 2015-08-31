b29427562288e4acf02fa8abc32a4dde136b685a
Christian <christianrober@gmail.com>
2015-08-31 16:39:00 -0400

    Added new license header to PITR.

--- pitr.cpp
+++ pitr.cpp
@@ -1,20 +1,19 @@
 // @file pitr.cpp
-
-/**
-*    Copyright (C) 2013 Tokutek Inc.
-*
-*    This program is free software: you can redistribute it and/or  modify
-*    it under the terms of the GNU Affero General Public License, version 3,
-*    as published by the Free Software Foundation.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU Affero General Public License for more details.
-*
-*    You should have received a copy of the GNU Affero General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/*======
+This file is part of Percona Server for MongoDB.
+Copyright (c) 2006, 2015, Percona and/or its affiliates. All rights reserved.
+    Percona Server for MongoDB is free software: you can redistribute
+    it and/or modify it under the terms of the GNU Affero General
+    Public License, version 3, as published by the Free Software
+    Foundation.
+    Percona Server for MongoDB is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public
+    License along with Percona Server for MongoDB.  If not, see
+    <http://www.gnu.org/licenses/>.  
+======= */
 
 #include "mongo/pch.h"
 
